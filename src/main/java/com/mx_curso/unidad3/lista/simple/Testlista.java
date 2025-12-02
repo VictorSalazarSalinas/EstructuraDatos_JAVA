eaf4d2e2f7a560751d074d8323a3d1c9ae3af64a
victor <victorsalazarsalinas@gmail.com>
2025-12-02 08:40:00 -0600

    ejerccios de Poo

---
+++ src/main/java/com/mx_curso/unidad3/lista/simple/Testlista.java
@@ -0,0 +1,20 @@
+package com.mx_curso.unidad3.lista.simple;
+
+public class Testlista {
+    public static void main(String[] args) {
+        ListaCabeza lista = new ListaCabeza();
+        System.out.println("insertar nuevos datos");
+
+        lista.insertarAlInicio(10);
+        lista.insertarAlInicio(20);
+        lista.insertarAlInicio(30);
+
+        System.out.println("insertar datos al final");
+        lista.insertarAlFinal(40);
+        lista.insertarAlFinal(50);
+
+
+        lista.insertarAlInicio(60);
+        lista.imprimir();
+    }
+}
